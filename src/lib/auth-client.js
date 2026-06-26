44f24295086643321cdde9aa19340c5daf5c45a5
manjur-moon <mmanjurulislam@gmail.com>
2026-06-26 20:56:12 +0600

    some fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,6 @@
-"use client";
-
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || undefined
-});
+export const authClient = createAuthClient();
 
-export const isGoogleAuthEnabled = process.env.NEXT_PUBLIC_ENABLE_GOOGLE_AUTH === "true";
+export const isGoogleAuthEnabled =
+  process.env.NEXT_PUBLIC_ENABLE_GOOGLE_AUTH === "true";
